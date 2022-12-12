8c0b3043c6d4166f443082d00e4d9a8652ccb7c8
Hugo Santos <hugo.santos@soapbox.sydney>
2022-12-13 05:27:54 +1100

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,17 +37,18 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
+          breadcrumbs: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/soapbox-rhizome/soapdox/edit/main/docs",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/soapbox-rhizome/soapdox/edit/main/blog",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
